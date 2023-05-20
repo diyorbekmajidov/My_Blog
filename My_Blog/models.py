e642d1f4f3c16dae1cc4c93196200cd911896a3b
Diyorbek <majidovdiyorbek741@gmail.com>
2023-05-20 21:02:08 +0500

    my blog

--- My_Blog/models.py
+++ My_Blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = RichTextField(blank=True, null=True)
+    date = models.DateField(auto_now_add=True)
+    cotent_upload = RichTextUploadingField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- My_Blog/views.py
+++ My_Blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post,Category
+from rest_framework.views import APIView
+
 
 # Create your views here.
+
+def index(request):
+    my_post = Post.objects.all()
+    return render(request, 'blog/index.html', {'my_post': my_post})
+
+def table(request):
+    my_table = Category.objects.all()
+    return render(request , "blog/table.html/", {"my_table":my_table})
