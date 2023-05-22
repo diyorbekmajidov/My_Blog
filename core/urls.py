8b33195ca285637131cbf725822cfb3c6ea7ff5b
Diyorbek <majidovdiyorbek741@gmail.com>
2023-05-22 11:22:04 +0500

    template urls

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 from django.urls.conf import include
-from My_Blog.views import index,table
+from My_Blog.views import index
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -11,5 +11,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path("ckeditor/", include('ckeditor_uploader.urls')),
-    path("home/", table, name="home"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
