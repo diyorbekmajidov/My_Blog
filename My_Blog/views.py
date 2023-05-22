5eecabccd09831caa0dd01f5c752fb30e59d8a08
Diyorbek <majidovdiyorbek741@gmail.com>
2023-05-22 11:17:50 +0500

    my home

--- My_Blog/views.py
+++ My_Blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post,Category
+from .models import Post
 from rest_framework.views import APIView
 
 
@@ -9,6 +9,3 @@ def index(request):
     my_post = Post.objects.all()
     return render(request, 'blog/index.html', {'my_post': my_post})
 
-def table(request):
-    my_table = Category.objects.all()
-    return render(request , "blog/table.html/", {"my_table":my_table})
