a283ba9abdf3cb17ac684253b77e5fa5b65b6c7a
Diyorbek <majidovdiyorbek741@gmail.com>
2023-05-27 14:49:37 +0500

    get all post

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 from django.urls.conf import include
-from My_Blog.views import index
+from My_Blog.views import index, PostList
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path("ckeditor/", include('ckeditor_uploader.urls')),
+    path('api/', PostList.as_view(), name='api'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
