467970e99f8725cf79a2fa71c7b9fb930e21b875
Diyorbek <majidovdiyorbek741@gmail.com>
2023-07-24 18:17:40 +0500

    create user app and user register view

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from users import views as users_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('accounts/', include(('django.contrib.auth.urls', 'django.contrib.auth'), namespace='login')),
-    path('accounts/', include(('django.contrib.auth.urls', 'django.contrib.auth'), namespace='logout')),
+    path('register/', users_views.register, name='register'),
     
 ]
