bcb882f5dd49d2692152ed49bd3d5ae0a6e21ac8
Diyorbek <majidovdiyorbek741@gmail.com>
2023-07-24 16:13:10 +0500

    all user

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-post = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_created': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_created': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': post
+        'posts': Post.objects.all()
     } 
     return render(request, 'blog/home.html', context)
 
