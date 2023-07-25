e876fb177240e0a7e6516e9cd1c2acd74f1368d3
Diyorbek <majidovdiyorbek741@gmail.com>
2023-07-25 13:20:33 +0500

    log in

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from users import views as users_views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('accounts/', include(('django.contrib.auth.urls', 'django.contrib.auth'), namespace='login')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', users_views.register, name='register'),
+    
     
 ]
